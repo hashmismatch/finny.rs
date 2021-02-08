3668d981cefc37956a6e215afce238c75baf5cd6
Rudi Benkovic <rudi.benkovic@gmail.com>
2021-02-08 22:54:46 +0100

    untested core, alloc free timers

--- finny/src/timers/core.rs
+++ finny/src/timers/core.rs
@@ -1,56 +1,96 @@
 //! An implementation of timers that relies just on the `Duration`. Has to be called with
 //! a reasonable period rate to trigger the timers.
 
-use crate::{FsmBackend, FsmTimers};
+use crate::{FsmBackend, FsmTimers, TimersStorage, AllVariants};
 use crate::lib::*;
 use Duration;
 use arraydeque::{Array, ArrayDeque};
 
-pub struct TimersCore<F, Q>
+pub struct TimersCore<F, S, Q>
     where F: FsmBackend,
-          Q: Array<Item = <F as FsmBackend>::Timers>
+          Q: Array<Item = <F as FsmBackend>::Timers>,
+          S: TimersStorage<<F as FsmBackend>::Timers, CoreTimer>
 {
-    timers: Vec<(<F as FsmBackend>::Timers, CoreTimer)>,
-    pending_events: ArrayDeque<Q>
-    //pending_intervals: Option<(<F as FsmBackend>::Timers, usize)>
+    timers: S,
+    pending_events: ArrayDeque<Q>,
+    _fsm: PhantomData<F>
 }
 
 #[derive(Debug)]
-enum CoreTimer {
+pub enum CoreTimer {
     Timeout { time_remaining: Duration },
+    Interval { time_remaining: Duration, interval: Duration }
 }
 
-impl<F, Q> TimersCore<F, Q>
+impl<F, S, Q> TimersCore<F, S, Q>
     where F: FsmBackend,
-    Q: Array<Item = <F as FsmBackend>::Timers>
+    Q: Array<Item = <F as FsmBackend>::Timers>,
+    S: TimersStorage<<F as FsmBackend>::Timers, CoreTimer>
 {
-    pub fn new() -> Self {
+    pub fn new(timers: S) -> Self {
         Self {
-            timers: vec![],
-            pending_events: ArrayDeque::new()
-            //pending_intervals: None
+            timers,
+            pending_events: ArrayDeque::new(),
+            _fsm: PhantomData::default()
         }
     }
 
     pub fn tick(&mut self, elapsed_since_last_tick: Duration) {
+        let iter = <F as FsmBackend>::Timers::iter();
+        for id in iter {
+            let mut timer = self.timers.get_timer_storage_mut(&id);
 
+            // todo: account for the difference between time remaining and elapsed time, currently we just reset it
+            match timer {
+                Some(CoreTimer::Timeout { time_remaining}) => {
+                    if *time_remaining <= elapsed_since_last_tick {
+                        self.pending_events.push_front(id);
+                        *timer = None
+                    } else {
+                        *time_remaining -= elapsed_since_last_tick;
+                    }
+                },
+                Some(CoreTimer::Interval { time_remaining, interval }) => {
+                    if *time_remaining <= elapsed_since_last_tick {
+                        self.pending_events.push_front(id);
+                        *time_remaining = *interval;
+                    } else {
+                        *time_remaining -= elapsed_since_last_tick;
+                    }
+                }
+                None => {}
+            }
+        }
     }
 }
 
-impl<F, Q> FsmTimers<F> for TimersCore<F, Q>
+impl<F, S, Q> FsmTimers<F> for TimersCore<F, S, Q>
     where F: FsmBackend,
-    Q: Array<Item = <F as FsmBackend>::Timers>
+    Q: Array<Item = <F as FsmBackend>::Timers>,
+    S: TimersStorage<<F as FsmBackend>::Timers, CoreTimer>
 {
     fn create(&mut self, id: <F as FsmBackend>::Timers, settings: &crate::TimerSettings) -> crate::FsmResult<()> {
-        todo!()
+        self.cancel(id.clone());
+
+        if settings.enabled {
+            let mut timer = self.timers.get_timer_storage_mut(&id);
+            if settings.renew {
+                *timer = Some(CoreTimer::Interval { interval: settings.timeout, time_remaining: settings.timeout });
+            } else {
+                *timer = Some(CoreTimer::Timeout { time_remaining: settings.timeout });
+            }
+        }
+        
+        Ok(())
     }
 
     fn cancel(&mut self, id: <F as FsmBackend>::Timers) -> crate::FsmResult<()> {
-        self.timers.retain(|(timer_id, _)| *timer_id != id);
+        let timer = self.timers.get_timer_storage_mut(&id);
+        *timer = None;
         Ok(())
     }
 
     fn get_triggered_timer(&mut self) -> Option<<F as FsmBackend>::Timers> {
-        todo!()
+        self.pending_events.pop_back()
     }
 }
--- finny/src/timers/mod.rs
+++ finny/src/timers/mod.rs
@@ -4,4 +4,4 @@ pub mod std_noalloc;
 #[cfg(feature="timers_std")]
 pub mod std;
 
-//pub mod core;
+pub mod core;
