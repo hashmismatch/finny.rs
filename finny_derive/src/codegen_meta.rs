f99cc2476ae651cebfd2e1211730f88528d6f05f
Rudi Benkovic <rudi.benkovic@gmail.com>
2022-01-10 23:44:52 +0100

    tests build fix

--- finny_derive/src/codegen_meta.rs
+++ finny_derive/src/codegen_meta.rs
@@ -155,7 +155,7 @@ pub fn generate_fsm_meta(fsm: &FsmFnInput) -> TokenStream {
 
                 impl #fsm_info_ty {
                     pub fn plantuml_inner() -> String {
-                        use alloc::fmt::Write;
+                        use std::fmt::Write;
 
                         let mut output = ( #plant_uml_str ).to_string();
 
@@ -165,7 +165,7 @@ pub fn generate_fsm_meta(fsm: &FsmFnInput) -> TokenStream {
                     }
 
                     pub fn plantuml() -> String {
-                        use alloc::fmt::Write;
+                        use std::fmt::Write;
 
                         let mut output = String::new();
 
